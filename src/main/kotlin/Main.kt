d0884b890319864173b4b86062ae62e42f45d9d5
Alexey Soshin <alexey.soshin@gmail.com>
2019-09-01 10:13:26 +0100

    First HTTP endpoints

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,34 @@
-fun main() {
-    println("Hello Kotlin Web")
-}
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-// print("Python")
-// console.log("JavaScript")
+fun main() {
+    val port = 8080
+
+    val server = embeddedServer(Netty, port) {
+        install(ContentNegotiation) {
+            jackson{
+                enable(SerializationFeature.INDENT_OUTPUT)
+            }
+        }
+        routing {
+            get {
+                context.respond(mapOf("Welcome" to "our Cat Hotel"))
+            }
+            get("/{name}") {
+                val name = call.parameters["name"]
+
+                context.respond(mapOf("Cat name:" to name))
+            }
+        }
+    }
+
+    server.start()
+}
