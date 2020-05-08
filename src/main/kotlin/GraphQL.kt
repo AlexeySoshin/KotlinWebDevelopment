8a396cb85ad8b9f3636ee25e38a810b67991b8e6
Alexey Soshin <alexey.soshin@gmail.com>
2020-05-08 15:25:51 +0100

    Change queries order

--- src/main/kotlin/GraphQL.kt
+++ src/main/kotlin/GraphQL.kt
@@ -3,18 +3,17 @@ import cats.CatsService
 import com.apurebase.kgraphql.Context
 import com.apurebase.kgraphql.graphql
 import io.ktor.routing.Route
-import nidomiro.kdataloader.dsl.dataLoader
 
 fun Route.graphql(catsService: CatsService) = graphql {
-    query("cat") {
-        resolver { id: Int ->
-            catsService.findById(id)
-        }
-    }
     query("cats") {
         resolver { ctx: Context ->
             ctx[String::class]
             catsService.all()
+        }
+    }
+    query("cat") {
+        resolver { id: Int ->
+            catsService.findById(id)
         }
     }
 
